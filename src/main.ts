0897b6be49b436295f3e87108c3ddf13805e2dae
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-08-29 10:51:13 -0300

    health check added

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,7 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.listen(envs.port);
 
+  logger.log('Health Check configured');
   logger.log(`Payments Microservice running on port ${envs.port}`)
 }
 bootstrap();
